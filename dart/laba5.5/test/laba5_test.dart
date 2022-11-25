bb8ced4a43f12986578934b63d5a5df17d604bbe
EgorPanda <119073728+EgorPanda@users.noreply.github.com>
2022-11-26 01:49:07 +0700

    rcis1
    rcis1

---
+++ dart/laba5.5/test/laba5_test.dart
@@ -0,0 +1,8 @@
+import 'package:laba5/laba5.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
---
+++ dart/laba6.2/test/laba6_test.dart
@@ -0,0 +1,8 @@
+import 'package:laba6/laba6.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
